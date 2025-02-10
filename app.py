e71536e7952aad7b6450798155c7c2382f3d9dd6
Paul <pgwar2@gmail.com>
2025-02-10 11:31:10 -0700

    port 80

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def addnewselection():
 
 if __name__ == '__main__':
     print("Starting Flask app")
-    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=80, threaded=True)
