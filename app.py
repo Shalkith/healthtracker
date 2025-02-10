e51d074489207d1c6346100a9393d2ab45e47858
Paul <pgwar2@gmail.com>
2025-02-10 11:30:12 -0700

    add mariadb runner instead of duckdb

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template,request,redirect
-from scripts import duckdb_runner
+#from scripts import duckdb_runner
+from scripts import mariadb_runner
 from datetime import datetime
 
-
+db_runner = mariadb_runner 
 
 
 app = Flask(__name__)
@@ -12,22 +13,22 @@ def main():
     if request.method == "GET":
         now = datetime.now()
         date = now.strftime("%Y-%m-%d %H:%M:%S")
-        #users = duckdb_runner.get_users()
-        #health_log = duckdb_runner.get_health_log()
-        con = duckdb_runner.connect()
-        selections = duckdb_runner.get_data(con,'select * from selections')
-        users = duckdb_runner.get_data(con,'select * from users')
+        #users = db_runner.get_users()
+        #health_log = db_runner.get_health_log()
+        con = db_runner.connect()
+        selections = db_runner.get_data(con,'select * from selections')
+        users = db_runner.get_data(con,'select * from users')
         #print(selections)
-        duckdb_runner.close_connection(con)
+        db_runner.close_connection(con)
         return render_template('index.html',date = date, selections=selections,users=users)
     
     if request.method == "POST":
-        con = duckdb_runner.connect()
+        con = db_runner.connect()
         selection = request.form['option']
         user = request.form['name']
         date = request.form['date']
-        duckdb_runner.insert_health_log(con,date,selection,user)
-        duckdb_runner.close_connection(con)
+        db_runner.insert_health_log(con,date,selection,user)
+        db_runner.close_connection(con)
         # redirect back to the index page
         return redirect("/")
 
@@ -38,32 +39,32 @@ def main():
 def insert():
     if request.method == "GET":
         return render_template('insert.html')
-    con = duckdb_runner.connect()
+    con = db_runner.connect()
     selection = request.form['selection']
-    duckdb_runner.insert_selection(con,selection)
-    duckdb_runner.close_connection(con)
+    db_runner.insert_selection(con,selection)
+    db_runner.close_connection(con)
     return redirect("/")
 
 @app.route("/addnewuser",  methods=["GET", "POST"])    
 def addnewuser():
     if request.method == "GET":
         return render_template('addnew.html',option='user')
-    con = duckdb_runner.connect()
+    con = db_runner.connect()
     user = request.form['value']
-    duckdb_runner.insert_user(con,user)
-    duckdb_runner.close_connection(con)
+    db_runner.insert_user(con,user)
+    db_runner.close_connection(con)
     return redirect("/")
 
 @app.route("/addnewselection",  methods=["GET", "POST"])    
 def addnewselection():
     if request.method == "GET":
         return render_template('addnew.html',option='selection')
-    con = duckdb_runner.connect()
+    con = db_runner.connect()
     selection = request.form['value']
-    duckdb_runner.insert_selection(con,selection)
-    duckdb_runner.close_connection(con)
+    db_runner.insert_selection(con,selection)
+    db_runner.close_connection(con)
     return redirect("/")
 
 if __name__ == '__main__':
     print("Starting Flask app")
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
---
+++ scripts/mariadb_runner.py
@@ -0,0 +1,107 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+def connect():
+    user = 'username'
+    password = 'password'
+    host = 'host'
+    port = 'port'
+    db = 'dbname'
+
+
+    # connect to the # mariadb
+    engine = create_engine('mysql+mysqlconnector://' + user + ':' + password + '@' + host + ':' + port + '/' + db)
+    # run a test query
+    #result = engine.execute("select * from health_log")
+    connection = engine.connect()
+    return connection 
+
+
+def drop_tables(con):
+    # drop the table if it exists
+    con.execute(sqlalchemy.text("DROP TABLE IF EXISTS health_log"))
+    con.execute(sqlalchemy.text("DROP TABLE IF EXISTS users"))
+    con.execute(sqlalchemy.text("DROP TABLE IF EXISTS selections"))
+
+    con.commit()
+
+
+
+
+def create_tables(con):
+    # create a table and load data into it if it doesnt already exist
+    con.execute(sqlalchemy.text("CREATE TABLE if not exists health_log (date timestamp, recording text,user text)"))
+    con.execute(sqlalchemy.text("CREATE TABLE if not exists users (id int  primary key AUTO_INCREMENT, name text)"))
+    con.execute(sqlalchemy.text("CREATE TABLE if not exists selections (selection text)"))
+    con.commit()
+
+
+def create_sequences(con):
+    # create a sequence
+    con.execute(sqlalchemy.text("CREATE SEQUENCE if not exists user_id start 1"))
+    con.commit()
+
+def insert_data(con):
+    # insert data into the table
+    con.execute(sqlalchemy.text("INSERT INTO health_log VALUES ('2021-01-01', 'protien', 'test_user')"))
+    con.execute(sqlalchemy.text("INSERT INTO users ( name) VALUES ('Michelle')"))
+    con.execute(sqlalchemy.text("INSERT INTO users ( name) VALUES ('Paul')"))
+    con.execute(sqlalchemy.text("INSERT INTO selections VALUES ('Protien')"))
+    con.execute(sqlalchemy.text("INSERT INTO selections VALUES ('Carbs')"))
+    con.execute(sqlalchemy.text("INSERT INTO selections VALUES ('Veggies')"))
+    con.commit()
+
+def insert_health_log(con, date, recording, user):
+    # insert data into the table
+    con.execute(sqlalchemy.text(f"INSERT INTO health_log VALUES ('{date}', '{recording}', '{user}')"))
+    con.commit()
+
+def insert_selection(con, selection):
+    # insert data into the table
+    con.execute(sqlalchemy.text(f"INSERT INTO selections VALUES ('{selection}')"))
+    con.commit()
+
+def insert_user(con, user):
+    # insert data into the table
+    con.execute(sqlalchemy.text(f"INSERT INTO users (name) VALUES ('{user}')"))
+    con.commit()
+
+def close_connection(con):
+    # explicitly close the connection
+    con.close()
+
+    
+
+
+def get_data(con,query):
+    list = []
+    result = con.execute(sqlalchemy.text(query))
+    #turn to list 
+    result = result.fetchall()
+    for row in result:
+        list.append(row)
+    return list
+
+    
+
+def setup():
+    
+    con = connect()
+    drop_tables(con)
+    create_tables(con)
+    create_sequences(con)
+
+    insert_data(con)
+    close_connection(con)
+
+if __name__ == '__main__':
+    #setup()
+    query = 'select * from health_log'
+    con = connect()
+    print(get_data(con,query))
+    close_connection(con)
+    print('done')
+
+        
