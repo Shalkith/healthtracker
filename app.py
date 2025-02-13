7a5696e4a338e29e4a5a1b4e1508ab65f64cb817
Paul <pgwar2@gmail.com>
2025-02-12 22:39:03 -0700

    update for web stats

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template,request,redirect, Response, flash
-#from scripts import duckdb_runner
 from scripts import mariadb_runner
 from datetime import datetime
-import matplotlib.pyplot as plt
-import io
 import numpy as np
-
+import plotly.graph_objects as go
+import io 
 db_runner = mariadb_runner 
 
 
@@ -18,12 +16,9 @@ def main():
     if request.method == "GET":
         now = datetime.now()
         date = now.strftime("%Y-%m-%d %H:%M:%S")
-        #users = db_runner.get_users()
-        #health_log = db_runner.get_health_log()
         con = db_runner.connect()
         selections = db_runner.get_data(con,'select * from selections')
         users = db_runner.get_data(con,'select * from users')
-        #print(selections)
         db_runner.close_connection(con)
         return render_template('index.html',date = date, selections=selections,users=users)
     
@@ -82,9 +77,89 @@ def stats(user):
     con = db_runner.connect()
     if request.method == "GET":
         #users = db_runner.get_data(con,'select * from users')
-        stats = db_runner.get_data(con,f"select * from health_log where user = '{user}'")
+        stats = db_runner.get_data(con,f"select * from health_log where user = '{user}' order by date desc")
         db_runner.close_connection(con)     
-        return render_template('stats.html',users=user,stats=stats)
+
+
+        data = {}
+        data['dates'] = {}
+        categories = set()
+
+        for row in stats:
+            date = row[1]
+            category = row[2]
+            print(row)
+            user = row[4]
+            try:
+                rowvalue = float(row[3])
+            except:
+                rowvalue = None
+            categories.add(category)  # Collect unique categories
+            if date in data['dates']:
+                if category in data['dates'][date]:
+                    #if value is not null, and its a number, sum it, else count it
+                    if rowvalue is not None and isinstance(rowvalue, (int, float)):
+                        data['dates'][date][category] += rowvalue
+                    else:
+                        data['dates'][date][category] += 1
+                else:
+                    #if value is not null, and its a number, sum it, else count it
+                    if rowvalue is not None and isinstance(rowvalue, (int, float)):
+                        data['dates'][date][category] = rowvalue
+                    else:
+                        data['dates'][date][category] = 1
+            else:
+                #if value is not null, and its a number, sum it, else count it
+                if rowvalue is not None and isinstance(rowvalue, (int, float)):
+                    data['dates'][date] = {category: rowvalue}
+                else:
+                    data['dates'][date] = {category: 1}
+
+        dates = list(data['dates'].keys())
+        carbs = [data['dates'][date].get('Carbs', 0) for date in dates]
+        veggies = [data['dates'][date].get('Veggies', 0) for date in dates]
+        protein = [data['dates'][date].get('Protein', 0) for date in dates]
+        lbs = [data['dates'][date].get('Lbs', 0) for date in dates]
+
+        # Create Plotly figure
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=dates, y=carbs, mode='lines+markers', name='Carbs'))
+        fig.add_trace(go.Scatter(x=dates, y=veggies, mode='lines+markers', name='Veggies'))
+        fig.add_trace(go.Scatter(x=dates, y=protein, mode='lines+markers', name='Protein'))
+
+        # Filter out zero values for weight (lbs)
+        filtered_dates = [date for date, weight in zip(dates, lbs) if weight != 0]
+        filtered_lbs = [weight for weight in lbs if weight != 0]
+        fig.add_trace(go.Scatter(x=filtered_dates, y=filtered_lbs, mode='lines+markers', name='Weight (lbs)',yaxis='y2'))
+
+        fig.update_layout(
+            title=f"Category Totals for {user}",
+            xaxis_title="Date & Time",
+            yaxis=dict(title="Primary Axis Categories", side="left"),
+            yaxis2=dict(
+                title="Weight (lbs)", 
+                side="right", 
+                overlaying='y', 
+                showgrid=False,
+                range=[0, max(filtered_lbs) + 10],  # Adjust range as needed
+            ),
+            xaxis=dict(tickangle=-45, type="date"),  # Keep datetime format
+            template="plotly_white",
+            legend=dict(title="Category",
+                        orientation="h",  # Set legend to be horizontal
+                        x=0.5,  # Center the legend horizontally
+                        xanchor="center",  # Anchor legend to the center
+                        y=1.1,  # Move legend above the chart (1.1 pushes it up)
+                        yanchor="bottom",  # Anchor legend to the bottom),
+        ))
+
+        # Convert Plotly figure to JSON
+        graph_json = fig.to_json()
+
+        #return render_template("chart.html", graph_json=graph_json)
+
+
+        return render_template('stats.html',users=user,stats=stats,graph_json=graph_json)
 
 
 @app.route("/stats/",  methods=["GET", "POST"])
@@ -102,87 +177,6 @@ def getstats():
 def index():
     flash("This is a toast message!", "success")  # Flash message with a category
     return render_template("toast.html")
-
-@app.route('/plot.png')
-def plot():
-    # Sample data (replace with your data)
-    dates = ["2024-01-01", "2024-02-01", "2024-03-01"]
-    weights = [180, 175, 170]
-
-    plt.figure(figsize=(6,4))
-    plt.plot(dates, weights, marker='o', linestyle='-')
-    plt.xlabel("Date")
-    plt.ylabel("Weight (lbs)")
-    plt.title("Weight Over Time")
-
-    # Save plot to a BytesIO object
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plt.close()
-
-    return Response(img.getvalue(), mimetype='image/png')
-
-@app.route('/<user>/bar_chart.png')
-def bar_chart(user):
-    con = db_runner.connect()
-    stats = db_runner.get_data(con,f"select * from health_log where user = '{user}' order by date asc")
-    db_runner.close_connection(con)     
-    data = {}
-    data['dates'] = {}
-
-    # Sample data (Dates and counts of instances for Carbs and Veggies)
-    #dates = ["2024-01", "2024-02", "2024-03"]
-    
-    for row in stats:
-        date = row[1]
-        category = row[2]
-        user = row[4]
-        if date in data['dates']:
-            if category in data['dates'][date]:
-                data['dates'][date][category] += 1
-            else:
-                data['dates'][date][category] = 1
-        else:
-            data['dates'][date] = {category: 1}
-    print(data)
-    dates = list(data['dates'].keys())
-    carbs = [data['dates'][date].get('Carbs', 0) for date in dates]
-    veggies = [data['dates'][date].get('Veggies', 0) for date in dates]
-    protein = [data['dates'][date].get('Protein', 0) for date in dates]
-    # line plot this data
-    print(dates)
-    print(carbs)
-    print(veggies)
-    print(protein)
-
-
-    #plt.figure(figsize=(10,4))
-    # needs to be able to show all the data and dates
-    plt.figure(figsize=(10,6))
-
-    plt.plot(dates, carbs, marker='o', linestyle='-', label='Carbs')
-    plt.plot(dates, veggies, marker='o', linestyle='-', label='Veggies')
-    plt.plot(dates, protein, marker='o', linestyle='-', label='Protein')
-
-    # order the x-axis
-    plt.xticks(dates, rotation=45)
-
-
-    
-    plt.xlabel("Date")
-    plt.ylabel("Count")
-    plt.title(f"Category Counts for {user}")
-    plt.legend()
-
-    # Save plot to a BytesIO object
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plt.close()
-
-
-    return Response(img.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
     print("Starting Flask app")
